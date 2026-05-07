d5b6bf6bdabdc507ae053203d37ba6063bdbb23c
Oscar <oskar_prp94@hotmail.com>
2026-05-07 17:31:38 +0200

    Examen

--- MarioAngular/MarioDex/karma.conf.js
+++ MarioAngular/MarioDex/karma.conf.js
@@ -1,10 +1,33 @@
 module.exports = function (config) {
   config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+    ],
+    client: {
+      jasmine: {},
+      clearContext: false,
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true,
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/mini-pokedex'),
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'text-summary' }],
+    },
+    reporters: ['progress', 'kjhtml'],
+    browsers: ['ChromeHeadlessNoSandbox'],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-gpu'],
+        flags: ['--no-sandbox', '--headless', '--disable-gpu', '--disable-dev-shm-usage'],
       },
     },
+    restartOnFileChange: true,
   });
 };
